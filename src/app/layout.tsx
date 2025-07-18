f8cd780650b1d2ccfd06202e52d7991a8fb1c3e4
Maxim Slobodchikov <maxims@bu.edu>
2025-07-17 23:58:19 -0400

    favicons mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,6 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "502 Whiteboard",
   description: "the whiteboard in room 502",
-  icons: {
-    icon: '/favicon.png',
-  },
 };
 
 export default function RootLayout({
@@ -16,6 +13,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Favicons & Manifest */}
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body>
         {children}
       </body>
